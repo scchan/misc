92fe82893287f970db406437cf5f0121a0d6273b
Siu Chi Chan <siuchi.chan@amd.com>
2020-06-11 02:36:37 +0000

    update script to build rocdl

--- linux/build_rocm_comp.pl
+++ linux/build_rocm_comp.pl
@@ -13,9 +13,8 @@ sub run_command {
         , $working_dir
         , $dry_run) = @_;
     if ($dry_run) {
-        print($command);
-    }
-    {
+        print("$command \n");
+    } else {
         my $dir;
         if ($working_dir) {
             $dir = pushd($working_dir);
@@ -39,8 +38,7 @@ my $cmake_command_return = "";
 my $current_path = cwd;
 my $rocm_path = "/opt/rocm";
 
-my $component_to_build = "llvm";
-
+my $components_to_build = "llvm";
 
 # AMD llvm package
 my $llvm_package_name = "llvm-amdgpu";
@@ -50,11 +48,19 @@ my $llvm_install_path = "$rocm_path/llvm";
 my $llvm_source_path = "$current_path/llvm-project";
 my $llvm_build_path = "";
 
-GetOptions ('gen=s' => \$cmake_generator
-            ,'build=s' => \$cmake_build_type
-            ,'llvm_source=s' => \$llvm_source_path
-            ,'llvm_build=s' => \$llvm_build_path
+# ROCm device libs
+my $rocdl_install_path = "$rocm_path";
+my $rocdl_source_path = "$current_path/ROCm-Device-Libs";
+my $rocdl_build_path = "";
+
+GetOptions ("build=s" => \$cmake_build_type
+            ,"components=s" => \$components_to_build
             ,"dry_run" => \$dry_run
+            ,"gen=s" => \$cmake_generator
+            ,"llvm_source=s" => \$llvm_source_path
+            ,"llvm_build=s" => \$llvm_build_path
+            ,"rocdl_source=s" => \$rocdl_source_path
+            ,"rocdl_build=s" => \$rocdl_build_path
             );
 
 
@@ -76,12 +82,13 @@ if ($lsb_release) {
     }
 }
 if ($cpack_generator) {
-    $cmake_command .= " -DCPACK_GENERATOR=$cpack_generator"
+    $cmake_command .= " -DCPACK_GENERATOR=$cpack_generator";
 }
 
-if ($component_to_build eq "llvm") {
 
-    # LLVM specific
+# LLVM specific
+if ($components_to_build eq "llvm") {
+
     $cmake_command .= " -DCPACK_PACKAGE_NAME=$llvm_package_name";
     $cmake_command .= " -DCMAKE_INSTALL_PREFIX=$llvm_install_path -DCPACK_PACKAGE_INSTALL_PREFIX=$llvm_install_path";
     $cmake_command .= " -DLLVM_TARGETS_TO_BUILD=\"$llvm_targets\"";
@@ -99,4 +106,28 @@ if ($component_to_build eq "llvm") {
     run_command($cmake_command, $llvm_build_path, $dry_run);
 }
 
+# ROCDL specific
+if ($components_to_build eq "rocdl") {
+
+    # add search path for clang
+    my $cmake_prefix_path = "/opt/rocm/llvm";
+    if ($llvm_build_path) {
+        $cmake_prefix_path = $llvm_build_path; 
+    }
+    $cmake_command .= " -DCMAKE_PREFIX_PATH=$cmake_prefix_path";
+
+    $cmake_command .= " -DCMAKE_INSTALL_PREFIX=$rocdl_install_path";
+    $cmake_command .= " -DCPACK_PACKAGING_INSTALL_PREFIX=$rocdl_install_path";
+
+    $cmake_command .= " $rocdl_source_path";
+
+    if ($rocdl_build_path eq "") {
+        $rocdl_build_path = "$current_path/build.rocdl.$cmake_build_type";
+    }
+    if (not (-d $rocdl_build_path)) {
+        mkdir($rocdl_build_path) or die "Unable to create $rocdl_build_path";
+    }
+
+    run_command($cmake_command, $rocdl_build_path, $dry_run);
+}
 # cmake -DCMAKE_BUILD_TYPE="Release" -DCMAKE_PREFIX_PATH=$LLVM_BUILD -DCMAKE_INSTALL_PREFIX=/opt/rocm/bitcode -DCPACK_PACKAGING_INSTALL_PREFIX=/opt/rocm/bitcode -DCPACK_GENERATOR=DEB  -DCPACK_PACKAGE_CONTACT=AMD   ../ROCm-Device-Libs/
